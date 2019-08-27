7e8b8e8e91bfc129797aa34b0fe2918450037422
Rajiv Singh <rajivpaulsingh@gmail.com>
2019-08-27 14:15:05 -0700

    Minor changes

--- src/main/java/qarajiv/App.java
+++ src/main/java/qarajiv/App.java
@@ -8,6 +8,12 @@ public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
+        System.out.println("Command to create the maven project is:");
+        System.out.println("mvn archetype:generate -DgroupId=com.rajiv -DartifactId=Mavenjava -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false");
+
+        System.out.println("And then goto that project folder and make it compatible for intellij by running:");
+        System.out.println("mvn idea:idea");
     }
 }
